0ea2f9b688f0f7be89a2abcc425ca00cac51ffa9
Reiaz Gafar <reiazgafar@ac.c4q.nyc>
2018-01-16 16:39:24 -0500

    add folders

---
+++ HereAndThere/HereAndThere/Models/Model.swift
@@ -0,0 +1,9 @@
+//
+//  Model.swift
+//  HereAndThere
+//
+//  Created by Reiaz Gafar on 1/16/18.
+//  Copyright © 2018 HereAndThere. All rights reserved.
+//
+
+import Foundation
---
+++ HereAndThere/HereAndThere/Views/View.swift
@@ -0,0 +1,21 @@
+//
+//  View.swift
+//  HereAndThere
+//
+//  Created by Reiaz Gafar on 1/16/18.
+//  Copyright © 2018 HereAndThere. All rights reserved.
+//
+
+import UIKit
+
+class View: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
