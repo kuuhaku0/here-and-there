aa7dc3d9835ebe693fbd5e9d76b93b55398a9a25
Ashlee Krammer <ashlee.alyse@me.com>
2018-01-24 14:29:05 -0500

    Implemented SnapKit

--- HereAndThere/Views/DetailedView.swift
+++ HereAndThere/Views/DetailedView.swift
@@ -11,9 +11,7 @@ import SnapKit
 
 class DetailedView: UIView {
 
-    //large title
     //picture
-    
     lazy var imageView: UIImageView = {
         let imageV = UIImageView() //default image
         imageV.image = #imageLiteral(resourceName: "placeholder-image")
@@ -36,12 +34,7 @@ class DetailedView: UIView {
         return label
     }()
     
-//    lazy var navButton: UINavigationItem = {
-//       let navButton = UINavigationItem()
-//        navButton.rightBarButtonItem?.image = #imageLiteral(resourceName: "plus")
-//        return navButton
-//    }()
-    
+
     override init(frame: CGRect) {
         super.init(frame: UIScreen.main.bounds)
         setUpView()
@@ -54,8 +47,8 @@ class DetailedView: UIView {
     
     func setUpView() {
         setUpImage()
-        setUpNotesLabel()
         setUpPlaceLabel()
+        setUpNotesLabel()
     }
     
     
@@ -63,49 +56,35 @@ class DetailedView: UIView {
     
     func setUpImage() {
         addSubview(imageView)
-//        imageView.translatesAutoresizingMaskIntoConstraints = false
-//        imageView.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: 20).isActive = true
-//        imageView.trailingAnchor.constraint(equalTo: safeAreaLayoutGuide.trailingAnchor).isActive = true
-//        imageView.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor).isActive = true
-//        imageView.heightAnchor.constraint(equalTo: heightAnchor, multiplier: 0.65).isActive = true
-        //        imageView.centerXAnchor.constraint(equalTo: safeAreaLayoutGuide.centerXAnchor).isActive = true
-        //    }
-        
+
         imageView.snp.makeConstraints { (make) -> Void in
             make.top.equalTo(safeAreaLayoutGuide).offset(20)
-            make.left.equalTo(safeAreaLayoutGuide).offset(8)
-            make.right.equalTo(safeAreaLayoutGuide).offset(-8)
+            make.left.equalTo(safeAreaLayoutGuide).offset(10)
+            make.right.equalTo(safeAreaLayoutGuide).offset(-10)
             make.height.equalTo(self).multipliedBy(0.65)
+            make.centerX.equalTo(self.snp.centerX)
         }
     }
     
     func setUpPlaceLabel() {
         addSubview(placeLabel)
-//        placeLabel.translatesAutoresizingMaskIntoConstraints = false
-//        placeLabel.trailingAnchor.constraint(equalTo: safeAreaLayoutGuide.trailingAnchor).isActive = true
-//        placeLabel.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor).isActive = true
-//        placeLabel.bottomAnchor.constraint(equalTo: notesLabel.topAnchor).isActive = true
-//        placeLabel.centerXAnchor.constraint(equalTo: safeAreaLayoutGuide.centerXAnchor).isActive = true
-        
+
         placeLabel.snp.makeConstraints { (make) -> Void in
-            make.top.equalTo(imageView).offset(10)
-            make.left.equalTo(safeAreaLayoutGuide).offset(8)
-            make.right.equalTo(safeAreaLayoutGuide).offset(-8)
+            make.top.equalTo(imageView.snp.bottom).offset(10)
+            make.leading.equalTo(safeAreaLayoutGuide).offset(10)
+            make.trailing.equalTo(safeAreaLayoutGuide).offset(-10)
+            make.centerX.equalTo(self.snp.centerX)
         }
     }
     
     func setUpNotesLabel() {
         addSubview(notesLabel)
-//        notesLabel.translatesAutoresizingMaskIntoConstraints = false
-//        notesLabel.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor).isActive = true
-//        notesLabel.trailingAnchor.constraint(equalTo: safeAreaLayoutGuide.trailingAnchor).isActive = true
-//        notesLabel.bottomAnchor.constraint(equalTo: safeAreaLayoutGuide.bottomAnchor, constant: -20).isActive = true
-//        notesLabel.centerXAnchor.constraint(equalTo: safeAreaLayoutGuide.centerXAnchor).isActive = true
-        
+
         notesLabel.snp.makeConstraints { (make) -> Void in
-            make.top.equalTo(placeLabel).offset(5)
-            make.left.equalTo(safeAreaLayoutGuide).offset(8)
-            make.right.equalTo(safeAreaLayoutGuide).offset(-8)
+            make.top.equalTo(placeLabel.snp.bottom).offset(5)
+            make.leading.equalTo(safeAreaLayoutGuide).offset(10)
+            make.trailing.equalTo(safeAreaLayoutGuide).offset(-10)
+            make.centerX.equalTo(self.snp.centerX)
         }
     }
 
